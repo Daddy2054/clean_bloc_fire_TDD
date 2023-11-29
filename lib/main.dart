659ba6f94663ee08e776d3bdd5303a1c359c4a1d
Jean <acalto2@gmail.com>
2023-11-29 23:18:03 +0100

    1.16: Presentation, Screens: Sign Up Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'firebase_options.dart';
+import 'src/features/auth/data/data_sources/auth_local_data_source.dart';
+import 'src/features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'src/features/auth/data/data_sources/auth_remote_data_source_firebase.dart';
+import 'src/features/auth/data/repositories/auth_repository_impl.dart';
+import 'src/features/auth/domain/entities/auth_user.dart';
+import 'src/features/auth/domain/repositories/auth_repository.dart';
+import 'src/features/auth/presentation/screens/sign_up_screen.dart';
 
 typedef AppBuilder = Future<Widget> Function();
 
@@ -17,32 +25,44 @@ Future<void> bootstrap(AppBuilder builder) async {
 void main() {
   bootstrap(
     () async {
-      return const App();
+      AuthLocalDataSource authLocalDataSource = AuthLocalDataSource();
+      AuthRemoteDataSource authRemoteDataSource =
+          AuthRemoteDataSourceFirebase();
+
+      AuthRepository authRepository = AuthRepositoryImpl(
+        localDataSource: authLocalDataSource,
+        remoteDataSource: authRemoteDataSource,
+      );
+
+      return App(
+        authRepository: authRepository,
+        authUser: await authRepository.authUser.first,
+      );
     },
   );
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({
+    super.key,
+    required this.authRepository,
+    required this.authUser,
+  });
+
+  final AuthRepository authRepository;
+  final AuthUser? authUser;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Clean Architecture & TDD',
-      theme: ThemeData.light(useMaterial3: true),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Clean Architecture & TDD')),
-      body: const Column(children: []),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: authRepository),
+      ],
+      child: MaterialApp(
+        title: 'Clean Architecture & TDD',
+        theme: ThemeData.light(useMaterial3: true),
+        home: const SignUpScreen(),
+      ),
     );
   }
 }

7d899a8e8390f964256529c0af8ba360169172b7
Jean <acalto2@gmail.com>
2023-12-01 22:00:59 +0100

    3.4: More about Theme: TextButtonTheme

--- lib/src/shared/theme/app_theme.dart
+++ lib/src/shared/theme/app_theme.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'app_color_scheme.dart';
 import 'app_elevated_button_theme.dart';
+import 'app_text_button_theme.dart';
 import 'app_text_theme.dart';
 
 class AppTheme {
   static final ThemeData theme = ThemeData.light(useMaterial3: true).copyWith(
-      colorScheme: AppColorScheme.appColorSchemeLight,
-      textTheme: appTextThemeLight,
-      elevatedButtonTheme: appElevatedButtonThemeLight);
+    colorScheme: AppColorScheme.appColorSchemeLight,
+    textTheme: appTextThemeLight,
+    elevatedButtonTheme: appElevatedButtonThemeLight,
+    textButtonTheme: appTextButtonThemeLight,
+  );
 
-  static final ThemeData darkTheme = ThemeData.dark(useMaterial3: true)
-      .copyWith(
-          colorScheme: AppColorScheme.appColorSchemeDark,
-          textTheme: appTextThemeDark,
-          elevatedButtonTheme: appElevatedButtonThemeDark);
+  static final ThemeData darkTheme =
+      ThemeData.dark(useMaterial3: true).copyWith(
+    colorScheme: AppColorScheme.appColorSchemeDark,
+    textTheme: appTextThemeDark,
+    elevatedButtonTheme: appElevatedButtonThemeDark,
+    textButtonTheme: appTextButtonThemeDark,
+  );
 }

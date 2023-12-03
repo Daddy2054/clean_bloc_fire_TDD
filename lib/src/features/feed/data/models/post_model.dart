e8aa391bdfde403ce359fa99d8e4c1370ee4365d
Jean <acalto2@gmail.com>
2023-12-03 21:32:59 +0100

    4.4: Data, Models: Post Model

---
+++ lib/src/features/feed/data/models/post_model.dart
@@ -0,0 +1,55 @@
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/post.dart';
+
+class PostModel extends Equatable {
+  final String id;
+  final String caption;
+  final String imageUrl;
+  final DateTime? createdAt;
+
+  const PostModel({
+    required this.id,
+    required this.caption,
+    required this.imageUrl,
+    this.createdAt,
+  });
+
+  factory PostModel.fromFakeDataSource(
+    Map<String, dynamic>? data, {
+    String? id,
+  }) {
+    return PostModel(
+      id: id ?? '',
+      caption: data?['caption'] ?? '',
+      imageUrl: data?['imageUrl'] ?? '',
+      createdAt: data?['createdAt'] != null
+          ? DateTime.parse(data?['createdAt'])
+          : null,
+    );
+  }
+
+  factory PostModel.fromCloudFirestore(
+    Map<String, dynamic>? data, {
+    String? id,
+  }) {
+    return PostModel(
+      id: id ?? '',
+      caption: data?['caption'] ?? '',
+      imageUrl: data?['imageUrl'] ?? '',
+      createdAt: data?['createdAt'].toDate(),
+    );
+  }
+
+  Post toEntity() {
+    return Post(
+      id: id,
+      caption: caption,
+      imageUrl: imageUrl,
+      createdAt: createdAt,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, caption, imageUrl, createdAt];
+}
